ac222760dda28eb22393e3cf230cd56eda789843
Ali Hyder <alihyder23@gmail.com>
2016-01-17 22:35:39 -0500

    Modified server.js

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/mesutozil', function(req, res){
     res.send('"Ya Gunners Ya" - Mesut Ozil');
+    res.attachment('/public/ozil.jpg');
 });
 app.listen(port, ipaddress);
