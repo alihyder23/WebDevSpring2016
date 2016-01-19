8674b89c0c6132c85067fb654d887f0e5d0fa9e9
Ali Hyder <alihyder23@gmail.com>
2016-01-19 12:38:51 -0500

    added files for Assignment 1

---
+++ public/assignment/js/yourScript.js
@@ -0,0 +1,3 @@
+/**
+ * Created by AliHyder on 1/19/16.
+ */
--- server.js
+++ server.js
@@ -10,6 +10,6 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/hello', function(req, res){
-    res.send('"hello world');
+    res.send('hello world');
 });
 app.listen(port, ipaddress);
