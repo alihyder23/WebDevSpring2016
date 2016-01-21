38f29e5363a30afef86fedba6613b08725e95185
Ali Hyder <alihyder23@gmail.com>
2016-01-21 15:27:45 -0500

    deleted hello.js

--- hello.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by AliHyder on 1/17/16.
- */
-console.log("hello world")
--- server.js
+++ server.js
@@ -9,7 +9,4 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
 app.listen(port, ipaddress);
