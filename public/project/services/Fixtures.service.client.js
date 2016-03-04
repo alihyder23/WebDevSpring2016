9a1757fddd58dc157a6fadc4e18b184c207577ea
Ali Hyder <alihyder23@gmail.com>
2016-03-03 20:37:05 -0500

    added calender list of games with result from API. Proof of Project commit

---
+++ public/project/services/Fixtures.service.client.js
@@ -0,0 +1,30 @@
+(function(){
+    'use strict';
+
+    var URL = 'http://api.football-data.org/v1/teams/57/fixtures/';
+
+    angular
+        .module("Gunners")
+        .factory("FixturesService", FixturesService);
+
+    function FixturesService($rootScope) {
+        var model = {
+            fixtures: [],
+            updateFixtures: updateFixtures
+        };
+        return model;
+
+        function updateFixtures(){
+            $.ajax({
+                headers: { 'X-Auth-Token': 'e7d0b480d8e64eb5828355511253d108' },
+                url: URL,
+                dataType: 'json',
+                type: 'GET',
+            }).done(function(response) {
+                var data = angular.fromJson(response)
+                model.fixtures = data.fixtures;
+                //console.log(model.fixtures)
+            });
+        }
+    }
+})();
