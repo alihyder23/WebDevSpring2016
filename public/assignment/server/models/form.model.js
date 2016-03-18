9cb248f874f4d14fa0ef976422fc5d8b1c0356f3
Ali Hyder <alihyder23@gmail.com>
2016-03-18 18:40:35 -0400

    managing scope nad some styling stuff. making sure you cant acces certain opages when no user is set. getting bug when adding fields to new form.

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -1,4 +1,5 @@
 var mock = require("./form.mock.json");
+var q = require("q");
 
 module.exports = function() {
     "use strict";
@@ -20,48 +21,65 @@ module.exports = function() {
     return api;
 
     function createForm(form) {
+        var deferred = q.defer();
+
         form._id = (new Date).getTime().toString();
         mock.push(form);
-        return form;
+        deferred.resolve(form);
+        return deferred.promise;
     }
 
     function findAllForms() {
-        return mock;
+        var deferred = q.defer();
+        deferred.resolve(mock);
+        return deferred.promise;
     }
 
     function findFormById(formId) {
+        var deferred = q.defer();
+
         for (var i in mock) {
             if (mock[i]._id === formId) {
-                return mock[i];
+                deferred.resolve(mock[i]);
+                return deferred.promise;
             }
         }
         return null;
     }
 
     function findFormByTitle(title) {
+        var deferred = q.defer();
+
         for (var i in mock) {
             if (mock[i].title === title) {
-                return mock[i];
+                deferred.resolve(mock[i]);
+                return deferred.promise;
             }
         }
         return null;
     }
 
     function findFormByUserId(userId) {
+        var deferred = q.defer();
+
         var userForms = [];
         for (var i in mock) {
             if (mock[i].userId === userId) {
                 userForms.push(mock[i])
             }
         }
-        return userForms;
+        deferred.resolve(userForms);
+        return deferred.promise;
     }
 
     function updateForm(formId, form) {
+        var deferred = q.defer();
+
         for (var i in mock) {
             if (mock[i]._id === formId) {
                 mock[i] = form;
-                return mock[i];
+                deferred.resolve(mock[i]);
+                return deferred.promise;
             }
         }
         return null;
@@ -78,24 +96,32 @@ module.exports = function() {
     }
 
     function findAllFieldsByFormId(formId) {
+        var deferred = q.defer();
+
         var form = findFormById(formId)
         if (form) {
-            return form.fields;
+            deferred.resolve(form.fields);
+            return deferred.promise;
         }
         return null;
     }
 
     function findFieldByFormId(formId, fieldId) {
+        var deferred = q.defer();
+
         var fields = findAllFieldsByFormId(formId);
         for (var i in fields) {
             if (fields[i]._id === fieldId) {
-                return fields[i];
+                deferred.resolve(fields[i]);
+                return deferred.promise;
             }
         }
         return null;
     }
 
     function deleteFieldById(formId, fieldId) {
+        var deferred = q.defer();
+
         var newFields = [];
         var currentFields = findAllFieldsByFormId(formId);
         for (var i in currentFields) {
@@ -107,19 +133,23 @@ module.exports = function() {
         for (var i in mock) {
             if (mock[i]._id === formId) {
                 mock[i].fields = newFields;
-                return mock[i].fields;
+                deferred.resolve(mock[i].fields);
+                return deferred.promise;
             }
         }
         return null;
     }
 
     function updateFieldById(formId, fieldId, newField) {
+        var deferred = q.defer();
+
         for (var i in mock) {
             if (mock[i]._id === formId) {
                 for (var j in mock[i].fields) {
                     if (mock[i].fields[j]._id === fieldId) {
                         mock[i].fields[j] = newField;
-                        return mock[i].fields[j];
+                        deferred.resolve(mock[i].fields[j]);
+                        return deferred.promise;
                     }
                 }
             }
@@ -128,21 +158,27 @@ module.exports = function() {
     }
 
     function updateAllFields(formId, newFields) {
+        var deferred = q.defer();
+
         for (var i in mock) {
             if (mock[i]._id === formId) {
                 mock[i].fields = newFields;
-                return mock[i].fields;
+                deferred.resolve(mock[i].fields);
+                return deferred.promise;
             }
         }
         return null;
     }
 
     function createFieldById(formId, newField) {
+        var deferred = q.defer();
+
         newField._id = (new Date).getTime().toString();
         for (var i in mock) {
             if (mock[i]._id === formId) {
                 mock[i].fields.push(newField);
-                return mock[i].fields;
+                deferred.resolve(mock[i].fields);
+                return deferred.promise;
             }
         }
         return null;
