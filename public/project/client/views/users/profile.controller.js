42bafe8619d3929c58dedae428b6849a4920d766
Ali Hyder <alihyder23@gmail.com>
2016-03-22 23:34:58 -0400

    fixed profile updating bug

--- public/project/client/views/users/profile.controller.js
+++ public/project/client/views/users/profile.controller.js
@@ -7,23 +7,16 @@
 
     function ProfileController($scope, $rootScope, UserService, $location) {
         $scope.message = null;
-
         if (!$scope.currentUser) {
             $location.url("/home");
         }
 
-        $scope.updates = {
-            username: $rootScope.currentUser.username,
-            password: $rootScope.currentUser.password,
-            firstName: $rootScope.currentUser.firstName,
-            lastName: $rootScope.currentUser.lastName,
-            email: $rootScope.currentUser.email
-        };
 
         $scope.update = update;
 
-        function update() {
-            UserService.updateUser($rootScope.currentUser._id, $scope.updates).then(function(res) {
+        function update(user) {
+            console.log(user);
+            UserService.updateUser($rootScope.currentUser._id, user).then(function(res) {
                 UserService.setCurrentUser(res.data);
                 $scope.message = "User updated successfully"
             });
