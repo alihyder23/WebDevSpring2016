16e891435abcd75bcea8fd37e2ce4d991e98bce1
Ali Hyder <alihyder23@gmail.com>
2016-04-17 19:03:16 -0400

    trying to get session to work on openshift

--- public/project/client/services/user.service.client.js
+++ public/project/client/services/user.service.client.js
@@ -59,15 +59,15 @@
         }
 
         function login(user) {
-            return $http.post('/api/assignment/login', user);
+            return $http.post('/api/project/login', user);
         }
 
         function register(user) {
-            return $http.post('/api/assignment/register', user);
+            return $http.post('/api/project/register', user);
         }
 
         function logout() {
-            return $http.post('/api/assignment/logout');
+            return $http.post('/api/project/logout');
         }
 
     }
